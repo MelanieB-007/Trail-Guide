db6f238c6461ef61fea55068fa3825583b247e25
Melanie <git@melanie-busse.de>
2026-05-12 16:26:42 +0200

    insert region controller and views

--- src/models/regionModel.ts
+++ src/models/regionModel.ts
@@ -1,11 +1,19 @@
 import { getDb } from "./db.js";
 
+// Holt alle Regionen für die Übersicht
 export async function getAllRegions() {
   const db = await getDb();
   return db.all("SELECT * FROM regions ORDER BY name ASC");
 }
 
+// Holt eine Region anhand ihres Slugs
 export async function getRegionBySlug(slug: string) {
   const db = await getDb();
   return db.get("SELECT * FROM regions WHERE slug = ?", [slug]);
+}
+
+// Holt alle Trails, die zu einer bestimmten Region gehören
+export async function getTrailsByRegionId(regionId: number) {
+  const db = await getDb();
+  return db.all("SELECT * FROM trails WHERE region_id = ?", [regionId]);
 }
--- src/routes/webRoutes.ts
+++ src/routes/webRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as trailController from "../controllers/trailController.js";
+import * as regionController from "../controllers/regionController.js";
 
 const router = Router();
 
@@ -8,5 +9,9 @@ router.get("/", trailController.renderAllTrails);
 
 // Detailseite (mit Variable :slug)
 router.get("/trails/:slug", trailController.renderTrailDetail);
+
+//Regionen
+router.get("/regions", regionController.renderAllRegions);
+router.get("/regions/:slug", regionController.renderRegionDetail);
 
 export default router;
